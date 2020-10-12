0113766fe05a39f2dafc80603d7e91e56f042f63
houxingyi <1026043046@qq.com>
2020-10-12 14:46:07 +0800

    s

---
+++ blocklySD/js/raw_block_b.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['raw_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("raw（5）");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ blocklySD/js/raw_block_g.js
@@ -0,0 +1,6 @@
+Blockly.JavaScript['raw_block'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
